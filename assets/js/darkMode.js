74162f0091e275fb0b0fc3b1dfe0f31df2acfbcf
Rosana Celine <celine.rosana@gmail.com>
2025-03-23 18:46:22 -0300

    feat: add support page and keyboard installation page

--- assets/js/darkMode.js
+++ assets/js/darkMode.js
@@ -2,15 +2,23 @@ const btn = document.querySelector('.dark-mode-btn');
 const body = document.querySelector('body');
 const lupa = document.querySelector('.search-btn img');
 
-btn.onclick = function()
-{
-    this.classList.toggle('active')
-    body.classList.toggle('active')
+// Verificar o estado do tema ao carregar a página
+if (localStorage.getItem('darkMode') === 'enabled') {
+    body.classList.add('active');
+    btn.classList.add('active');
+    lupa.src = '../assets/icons/icon-Lupa-dark-mode.png';
+}
 
-    
+// Alternar entre os modos escuro e claro
+btn.onclick = function() {
+    this.classList.toggle('active');
+    body.classList.toggle('active');
+
     if (body.classList.contains('active')) {
-        lupa.src = '../assets/icons/icon-Lupa-dark-mode.png'; // Imagem para o modo escuro
+        localStorage.setItem('darkMode', 'enabled');
+        lupa.src = '../assets/icons/icon-Lupa-dark-mode.png';
     } else {
-        lupa.src = '../assets/icons/icon-Lupa-light-mode.png'; // Imagem para o modo claro
+        localStorage.setItem('darkMode', 'disabled');
+        lupa.src = '../assets/icons/icon-Lupa-light-mode.png';
     }
 }
