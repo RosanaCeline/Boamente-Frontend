571653e509393aba77e8dbc9671abbacaf0afca6
laisdeveloper <laiscc575@gmail.com>
2025-05-02 15:06:22 -0300

    feat: setup initial frontend react

---
+++ boamente-frontend/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <header>
+        <link rel="icon" href="%PUBLIC_URL%/favicon.ico" type="image/x-icon"></link>
+    </header>
+  );
+}
+
