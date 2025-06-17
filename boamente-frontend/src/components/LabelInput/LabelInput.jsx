3a9dfeb81861feb4614061848104e7325ef83068
laisdeveloper <laiscc575@gmail.com>
2025-06-17 10:31:26 -0300

    feat: adding Support Page

--- boamente-frontend/src/components/LabelInput/LabelInput.jsx
+++ boamente-frontend/src/components/LabelInput/LabelInput.jsx
@@ -1,43 +1,60 @@
 import React from 'react';
 import style from './LabelInput.module.css';
 
-export default function LabelInput({ id, name, type, placeholder, required, register, options, label, errors }) {
-  if (type === "select") {
-    return (
-      <div className={style.inputGroup}>
-        <label htmlFor={id} className={style.formLabel}>{label}</label>
-        <select
-          id={id}
-          name={name}
-          required={required}
-          className={style.formInputSelect}
-          {...(register ? register(name, { required }) : {})}
-        >
-          <option value="">Selecione uma opção</option>
-          {options?.map(option => (
-            <option key={option.value} value={option.value}>
-              {option.label}
-            </option>
-          ))}
-        </select>
-        {errors[name] && <span className={style.errorMessage}>{errors[name]?.message}</span>}
-      </div>
-    );
-  }
+export default function LabelInput({  id, name, type, placeholder, required, register, options, label, errors, showLabel = true, className = '' }) {
+  let InputElement = null;
 
-  return (
-    <div className={style.inputGroup}>
-      <label htmlFor={id} className={style.formLabel}>{label}</label>
+  if (type === "select") {
+    InputElement = (
+      <select
+        id={id}
+        name={name}
+        required={required}
+        className={`${style.formInputSelect} ${className}`}
+        {...(register ? register(name, { required }) : {})}
+      >
+        <option value="">Selecione uma opção</option>
+        {options?.map(option => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    );
+  } else if (type === "textarea") {
+    InputElement = (
+      <textarea
+        id={id}
+        name={name}
+        placeholder={placeholder}
+        required={required}
+        className={`${style.formInput} ${className}`}
+        {...(register ? register(name, { required }) : {})}
+      />
+    );
+  } else {
+    InputElement = (
       <input
         id={id}
         name={name}
         type={type}
         placeholder={placeholder}
         required={required}
-        className={style.formInput}
+        className={`${style.formInput} ${className}`}
         {...(register ? register(name, { required }) : {})}
       />
-      {errors[name] && <span className={style.errorMessage}>{errors[name]?.message}</span>}
+    );
+  }
+
+  return (
+    <div className={style.inputGroup}>
+      {showLabel && (
+        <label htmlFor={id} className={style.formLabel}>{label}</label>
+      )}
+      {InputElement}
+      {errors?.[name] && (
+        <span className={style.errorMessage}>{errors[name]?.message}</span>
+      )}
     </div>
   );
 }
--- boamente-frontend/src/pages/Support/Support.jsx
+++ boamente-frontend/src/pages/Support/Support.jsx
@@ -1,8 +1,51 @@
 import  React from 'react';
+import "../../style.css";
 import style from "./Support.module.css";
+import ButtonSubmit from '../../components/ButtonSubmit/ButtonSubmit.jsx';
+import LabelInput from '../../components/LabelInput/LabelInput.jsx';
 
 export default function Support () {
     return (
-        <h1>pagina de suporte</h1>
+        <div className={style.boxSupport}>
+            <h2>Envie uma Solicitação de Suportes</h2>
+            <form action="">
+                <section className={style.formGroup}>
+                    <label for="">Assunto 
+                        <span className={style.required}>*</span>
+                    </label>
+                    <LabelInput
+                        id="assunto"
+                        name="assunto"
+                        type="text"
+                        placeholder="Digite o assunto da sua solicitação"
+                        required={true}
+                        showLabel={false}
+                        className={style.customInputs}
+                    />
+                    <div className={style.erro}> 
+                        <span className={style.erroAssunto}>Limite de 70 caracteres excedido!</span>
+                    </div>
+
+                    <label for="">Descrição 
+                        <span className={style.required}>*</span>
+                    </label>
+                    <LabelInput
+                        id="assunto"
+                        name="assunto"
+                        type="textarea"
+                        placeholder="Digite aqui os detalhes..."
+                        required={true}
+                        showLabel={false}
+                        className={`${style.customInputs} ${style.customInputTextarea}`}
+                    />
+                    <div className={style.erro}> 
+                        <span className={style.erroDescricao}>Limite de 500 caracteres excedido!</span> 
+                    </div>
+                </section>
+
+                <span className={style.legenda}>Enviaremos a resposta da sua solicitação para seu e-mail em breve!</span>
+                <ButtonSubmit type="submit"> Enviar Solicitação </ButtonSubmit>
+            </form>
+        </div>
     );
 }
